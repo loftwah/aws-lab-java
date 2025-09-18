8639ac49519ffcd4749a38513f056f8405eb62dc
Dean Lofts <dean@deanlofts.xyz>
2025-09-19 00:54:56 +1000

    fix state

--- infrastructure/terraform/stacks/development/core-networking/variables.tf
+++ infrastructure/terraform/stacks/development/core-networking/variables.tf
@@ -21,3 +21,27 @@ variable "additional_tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "vpc_id" {
+  description = "Pre-provisioned VPC ID reused by the environment"
+  type        = string
+  default     = "vpc-075f21399ed8cdd47"
+}
+
+variable "public_subnet_ids" {
+  description = "Pre-provisioned public subnet IDs"
+  type        = list(string)
+  default = [
+    "subnet-0dd3f7f05ca1cb8d8",
+    "subnet-0b7e614d07d9f6030",
+  ]
+}
+
+variable "private_subnet_ids" {
+  description = "Pre-provisioned private subnet IDs"
+  type        = list(string)
+  default = [
+    "subnet-03d29cbce89aeaf14",
+    "subnet-081e4da6ba7b7046e",
+  ]
+}
